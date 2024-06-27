55562502abd8500ddc6007b2310ca67c8af7f4ff
thealhu <huthanslhd@gmail.com>
2024-06-27 20:18:47 +0200

    add fortunes.txt check for docker

--- docker/configini.sh
+++ docker/configini.sh
@@ -2,3 +2,6 @@
 if [ ! -f "/config/config.ini" ]; then
     cp "/TC2-BBS-mesh/config.ini" "/config/config.ini"
 fi
+if [ ! -f "/config/fortunes.txt" ]; then
+    cp "/TC2-BBS-mesh/fortunes.txt" "/config/fortunes.txt"
+fi
