a02534d90d401c9dfa5ed6349c8e3e61aee02119
Blergo <beardedowen@gmail.com>
2024-07-02 11:48:17 +0100

    trying to make server stats working

--- command_handlers.py
+++ command_handlers.py
@@ -115,8 +115,8 @@ def handle_stats_steps(sender_id, message, step, interface, bbs_nodes):
             send_message(response, sender_id, interface)
             update_user_state(sender_id, {'command': 'STATS', 'step': 2})
         if choice == 1:
-            cpu = print(psutil.cpu_percent())
-            response = "CPU: " + str(cpu) + "%"
+            cpu = str(psutil.cpu_percent())
+            response = "CPU: " + cpu + "%"
             send_message(response, sender_id, interface)
             handle_stats_command(sender_id, interface)
             return
