b8540af79293962a0fdbc72ecf8303abd3f65350
Blergo <beardedowen@gmail.com>
2024-07-02 20:53:33 +0100

    changed from CPU % to load average

--- command_handlers.py
+++ command_handlers.py
@@ -102,9 +102,9 @@ def handle_stats_steps(sender_id, message, step, interface, bbs_nodes):
             send_message(response, sender_id, interface)
             update_user_state(sender_id, {'command': 'STATS', 'step': 2})
         if choice == 1:
-            cpu = str(psutil.cpu_percent(interval=1, percpu=False))
+            cpu = str(psutil.getloadavg())
             ramu = str(psutil.virtual_memory().percent)
-            response = "CPU: " + cpu + "% Used\nRAM: " + ramu + "% Used"
+            response = "CPU: " + cpu + "\nRAM: " + ramu + "% Used"
             send_message(response, sender_id, interface)
             handle_stats_command(sender_id, interface)
             return
