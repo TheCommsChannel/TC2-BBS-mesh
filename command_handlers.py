7a6b0f6720f1da674b4e29970fd979d6153acc1e
Blergo <beardedowen@gmail.com>
2024-07-02 20:34:51 +0100

    cpu util work

--- command_handlers.py
+++ command_handlers.py
@@ -102,7 +102,7 @@ def handle_stats_steps(sender_id, message, step, interface, bbs_nodes):
             send_message(response, sender_id, interface)
             update_user_state(sender_id, {'command': 'STATS', 'step': 2})
         if choice == 1:
-            cpu = str(psutil.cpu_percent(interval=1)/psutil.cpu_count())
+            cpu = str(psutil.cpu_percent(interval=1, percpu=False))
             ramu = str(psutil.virtual_memory().percent)
             response = "CPU: " + cpu + "% Used\nRAM: " + ramu + "% Used"
             send_message(response, sender_id, interface)
