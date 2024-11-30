ab228e97f0e8e6bec47143907d106a7f5cf2741b
Jacob Kern <jakern@users.noreply.github.com>
2024-11-30 18:32:48 -0500

    Rename example_config.ini for docker build
    resolves #80

--- docker/configini.sh
+++ docker/configini.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -f "/config/config.ini" ]; then
-    cp "/TC2-BBS-mesh/config.ini" "/config/config.ini"
+    cp "/TC2-BBS-mesh/example_config.ini" "/config/config.ini"
 fi
 if [ ! -f "/config/fortunes.txt" ]; then
     cp "/TC2-BBS-mesh/fortunes.txt" "/config/fortunes.txt"
